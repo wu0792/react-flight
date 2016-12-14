a5c8a1d3f7901227381ac24aafab395e73e0dcdb
wu0792 <wu0792@163.com>
2016-12-14 08:41:04 +0800

    添加输入框的错误提示功能

--- src/components/PassengerInput.js
+++ src/components/PassengerInput.js
@@ -6,21 +6,28 @@ class PassengerInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            hasError: true,
+            errorMsg: []
         };
     }
 
-    getId(){
+    getId() {
         return `passenger_input_${incId++}`;
     }
 
     render() {
         let id = this.getId();
-        let className = classNames(this.props.className)
+        let rootClassName = classNames({ 'form-error': this.state.hasError }, this.props.className)
+        let errorClassName = classNames('form-error-msg', { 'none': !this.state.hasError })
+
         return (
-            <div className={className}>
+            <div className={rootClassName}>
                 <input type="text" id={id} maxLength={this.props.maxLength || ''} className="form-input" />
                 <label htmlFor={id} className="form-input-hint">{this.props.placeholder}</label>
+                <div className={errorClassName} >
+                    <i className="ico-error"></i>
+                    <span>请填写正确的简体中文姓名，中文姓名的第一个字必须为汉字。</span>
+                </div>
             </div>
         );
     }
